afa361cc58633734aecfd1f3b54490176e32d8a4
Lolash <michalzwolaklol@gmail.com>
2016-05-04 18:41:56 +0200

    Test commit with client draft

---
+++ Client/src/Controller.java
@@ -0,0 +1,14 @@
+package client.radio.com;
+
+/**
+ * Created by Michał on 2016-05-04.
+ */
+public class Controller {
+    private Player player;
+    private Receiver receiver;
+    private Playlist playlist;
+    private Sender sender;
+    private Recorder recorder;
+
+
+}
