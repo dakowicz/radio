4a084f89686a1e811425c2aefff1b5ae93b0f41f
Lolash <michalzwolaklol@gmail.com>
2016-05-04 18:41:56 +0200

    Test commit with client draft

---
+++ Client/src/View.java
@@ -0,0 +1,13 @@
+package client.radio.com;
+
+/**
+ * Created by Michał on 2016-04-23.
+ */
+public class View {
+    private Player player;
+    private Receiver receiver;
+    private Playlist playlist;
+    private Sender sender;
+    private Recorder recorder;
+    private Controller controller;
+}
