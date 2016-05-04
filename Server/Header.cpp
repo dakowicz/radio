9523aa6b18969d0ac7260ad3fe2e97918af53a0c
Kamil <kamildeja@o2.pl>
2016-05-04 17:53:47 +0200

    Server protocol Header class

--- Server/Header.cpp
+++ Server/Header.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "Header.h"
+
+Header::Header(void){
+
+}
+Header::Header(uint8_t head[7]) {
+    if(head[0]!=id_prot||head[1]>3) {
+        cout << "Wrong protocol";
+        return;
+    }
+    type=head[1]; //Here is also the zero-tail
+    parameters=head[2];
+    length=head[3]<<24;
+    length+=head[4]<<16;
+    length+=head[5]<<8;
+    length+=head[6];
+}
+
+void Header::createHeader(uint8_t t, uint8_t param, int l){
+    type=t;
+    parameters=param;
+    length=l;
+
+}
+
+void Header::createHeaderConnect(bool start, bool end, int l){
+    type=connect;
+    parameters=(uint8_t)(start?1:0);
+    parameters+=(uint8_t)(end?2:0);
+    length=l;
+}
+void Header::createHeaderVote(bool cancel_vote, int l){
+    type=votes;
+    parameters=(uint8_t)(cancel_vote?1:0);
+    parameters+=2;	//Since it's the client to server message
+    length=l;
+}
+
+void Header::createHeaderList(bool ack, int l){
+    type=votes;
+    parameters=(uint8_t)(ack?1:0);
+    length=l;
+}
+
+void Header::createHeaderStream(bool start, bool end, int l){
+    type=votes;
+    parameters=(uint8_t)(start?1:0);
+    parameters+=(uint8_t)(end?2:0);
+    length=l;
+}
+
+void Header::createHeaderFile(bool priority, uint8_t info_length, int l){// It's probably not used by server
+    type=file;
+    if(info_length<0)
+        return;
+    parameters=info_length;
+    parameters+=(uint8_t)(priority?-128:0);
+    length=l;
+}
+
--- Server/Header.h
+++ Server/Header.h
@@ -1,13 +1,33 @@
 //
 // Created by Kamil on 2016-05-04.
 //
-
 #ifndef SERVER_HEADER_H
 #define SERVER_HEADER_H
 
+#import <cstdint>
+#import <iostream>
+
+using namespace std;
 
 class Header {
+public:
+    Header(void);
+    Header(uint8_t head[7]);
+    void createHeader(uint8_t t, uint8_t param, int l);
+    void createHeaderConnect(bool start, bool end, int l);
+    void createHeaderVote(bool cancel_vote, int l);
+    void createHeaderFile(bool priority, uint8_t info_length, int l);
+    void createHeaderList(bool cancel_vote, int l);
+    void createHeaderStream(bool start, bool end, int l);
 
+    const uint8_t connect=0;
+    const uint8_t stream=1;
+    const uint8_t votes=2;
+    const uint8_t file=3;
+    const uint8_t id_prot=2;
+    uint8_t type; //Here is also the zero-tail
+    uint8_t parameters;
+    int length;
 };
 
 
