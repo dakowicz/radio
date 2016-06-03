eab5bd77a986e96673c1341851657223ccd54f1b
Kamil <kamildeja@o2.pl>
2016-06-04 00:39:44 +0200

    Added Playlist handling parsing and so on, Controller small changes to handle Playlist

--- Client/src/main/java/client/radio/com/Playlist.java
+++ Client/src/main/java/client/radio/com/Playlist.java
@@ -1,10 +1,14 @@
 package client.radio.com;
 
+import com.sun.xml.internal.messaging.saaj.util.CharReader;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import org.apache.commons.csv.*;
 
 /**
  * Created by Michał on 2016-04-23.
@@ -14,13 +18,56 @@ import java.util.Set;
 @Slf4j
 public class Playlist {
 
-    private Set<Song> playlist;
+    public static char separator='\n';
+    private ConcurrentMap<Integer,Song> CurrentPlaylist;
+    private CSVParser parser;
+
+
 
     public Playlist() {
-        playlist = new HashSet<Song>();
+        CurrentPlaylist= new ConcurrentHashMap<Integer,Song>();
     }
 
-    public void vote(int songId) {
+    public void checkVoted(int songId) {
+        try {
+            CurrentPlaylist.get(songId).setVoted(true);
+        }catch(NullPointerException e){
+            log.info("No such a song");
+        }
 
+    }
+
+    public void checkUnvoted(int songId){
+        try {
+            CurrentPlaylist.get(songId).setVoted(false);
+        }catch(NullPointerException e){
+            log.info("No such a song");
+        }
+    }
+
+    private void createPlaylist(List<CSVRecord> list){
+        log.info(list.get(0).toString());
+        Iterator<CSVRecord> iterator=list.iterator();
+        while(iterator.hasNext()) {
+            CSVRecord record = iterator.next();
+            Song song = new Song(record.get(1), record.get(2), Integer.valueOf(record.get(0).trim()), Integer.valueOf(record.get(3).trim()));
+            updateOrPutSong(song);
+        }
+    }
+    public void updateOrPutSong(Song song){
+        if(CurrentPlaylist.containsKey(song))
+            CurrentPlaylist.replace(song.getId(),song);
+        else{
+            CurrentPlaylist.put(song.getId(),song);
+        }
+    }
+
+    public void handleNewPlaylist(byte[] data) throws IOException {
+        //StringReader buffer= new StringReader(data.toString());
+        log.info("Parsing new Playlist");
+        Reader targetReader = new InputStreamReader(new ByteArrayInputStream(data));
+        parser= new CSVParser(targetReader,CSVFormat.DEFAULT.withRecordSeparator(separator));
+        createPlaylist(parser.getRecords());
+        targetReader.close();
     }
 }
