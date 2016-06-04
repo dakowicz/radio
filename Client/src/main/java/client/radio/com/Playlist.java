4e14a6daabf7c5cb3af35c31421df6fb93f44438
Lolash <michalzwolaklol@gmail.com>
2016-06-04 09:03:54 +0200

    Add dependency, delete unnecessary JavaFX class, minor styling

--- Client/src/main/java/client/radio/com/Playlist.java
+++ Client/src/main/java/client/radio/com/Playlist.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+
 import org.apache.commons.csv.*;
 
 /**
@@ -18,47 +19,46 @@ import org.apache.commons.csv.*;
 @Slf4j
 public class Playlist {
 
-    public static char separator='\n';
-    private ConcurrentMap<Integer,Song> CurrentPlaylist;
+    public static char separator = '\n';
+    private ConcurrentMap<Integer, Song> CurrentPlaylist;
     private CSVParser parser;
 
 
-
     public Playlist() {
-        CurrentPlaylist= new ConcurrentHashMap<Integer,Song>();
+        CurrentPlaylist = new ConcurrentHashMap<Integer, Song>();
     }
 
     public void checkVoted(int songId) {
         try {
             CurrentPlaylist.get(songId).setVoted(true);
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             log.info("No such a song");
         }
-
     }
 
-    public void checkUnvoted(int songId){
+    public void checkUnvoted(int songId) {
         try {
             CurrentPlaylist.get(songId).setVoted(false);
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             log.info("No such a song");
         }
     }
 
-    private void createPlaylist(List<CSVRecord> list){
+    private void createPlaylist(List<CSVRecord> list) {
         log.info(list.get(0).toString());
-        Iterator<CSVRecord> iterator=list.iterator();
-        while(iterator.hasNext()) {
+        Iterator<CSVRecord> iterator = list.iterator();
+        while (iterator.hasNext()) {
             CSVRecord record = iterator.next();
             Song song = new Song(record.get(1), record.get(2), Integer.valueOf(record.get(0).trim()), Integer.valueOf(record.get(3).trim()));
             updateOrPutSong(song);
         }
     }
-    public void updateOrPutSong(Song song){
-        if(CurrentPlaylist.containsKey(song))
-            CurrentPlaylist.replace(song.getId(),song);
-        else{
-            CurrentPlaylist.put(song.getId(),song);
+
+    public void updateOrPutSong(Song song) {
+        if (CurrentPlaylist.containsKey(song))
+            CurrentPlaylist.replace(song.getId(), song);
+        else {
+            CurrentPlaylist.put(song.getId(), song);
         }
     }
 
@@ -66,7 +66,7 @@ public class Playlist {
         //StringReader buffer= new StringReader(data.toString());
         log.info("Parsing new Playlist");
         Reader targetReader = new InputStreamReader(new ByteArrayInputStream(data));
-        parser= new CSVParser(targetReader,CSVFormat.DEFAULT.withRecordSeparator(separator));
+        parser = new CSVParser(targetReader, CSVFormat.DEFAULT.withRecordSeparator(separator));
         createPlaylist(parser.getRecords());
         targetReader.close();
     }
